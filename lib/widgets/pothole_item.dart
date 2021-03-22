1423039bb93a02c2b492c1e70875102fbb3adbb8
abhishek7997 <68701271+abhishek7997@users.noreply.github.com>
2021-03-22 18:19:16 +0530

    Modified pothole_item.dart to display a small image in place of id for each pothole item

--- lib/widgets/pothole_item.dart
+++ lib/widgets/pothole_item.dart
@@ -8,36 +8,54 @@ class PotHoleItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final pothole = Provider.of<PotHole>(context, listen: false);
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.symmetric(vertical: 14.0),
       child: Container(
         decoration: BoxDecoration(
-            color: Colors.grey, borderRadius: BorderRadius.circular(15.0)),
+            color: Color(0xFFd8e2dc),
+            borderRadius: BorderRadius.circular(15.0)),
         child: ListTile(
           trailing: Icon(pothole.isFixed ? Icons.check : Icons.clear),
           title: Row(
+            // mainAxisSize: MainAxisSize.min,
             children: [
-              Text(
-                pothole.id,
-                style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 5.0),
+                // height: double.infinity,
+                alignment: Alignment.center,
+                child: Image.file(
+                  pothole.Image,
+                  fit: BoxFit.contain,
+                  width: 65,
+                  height: 65,
+                ),
               ),
               SizedBox(
-                width: 20.0,
+                width: 10.0,
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "LAT: ${pothole.Latitude}, LONG: ${pothole.Longitude}",
-                    style: TextStyle(
-                        fontSize: 14.0, fontWeight: FontWeight.normal),
-                  ),
-                  Text(
-                    "${pothole.Address}",
-                    style: TextStyle(
-                        fontSize: 12.0, fontWeight: FontWeight.normal),
-                  ),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "LAT: ${pothole.Latitude}, LONG: ${pothole.Longitude}",
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10.0),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      child: Text(
+                        "${pothole.Address}",
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontSize: 14.0, fontWeight: FontWeight.normal),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
